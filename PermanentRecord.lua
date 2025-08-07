d10fec85c21773939f154e30d7eca443c179edb5
argi <15852038+argium@users.noreply.github.com>
2025-08-07 17:17:54 +1000

    consolidate

--- PermanentRecord.lua
+++ PermanentRecord.lua
@@ -8,17 +8,23 @@ local comment = {
 }
 
 local record = {
-  playerId = "", -- player name
+  playerId = "",   -- player name
+  flag = 0,        -- flag color
+  comments = {},   -- list of comments
+  fingerprint = "" -- computed finger for the battle.net account
+}
+
+local guild = {
+  guildId = "",  -- guild name
   flag = 0,      -- flag color
   comments = {}, -- list of comments
-  fingerprint = "" -- computed finger for the battle.net account
 }
 
 local defaults = {
   profile = {
     debug = true, -- enable debug output
-    loaded = addonName,
-    records = {},
+    players = {},
+    guilds = {},
   },
 }
 
@@ -27,39 +33,44 @@ local defaults = {
 -- history
 
 
-function PR_LOG_INFO(...)
-  if PermanentRecord.db.profile.debug then
+function PermanentRecord:PR_LOG_TRACE(...)
+  if self.db.profile.debug then
     print("[PermanentRecord]", ...)
   end
 end
 
-function PR_LOG_WARN(...)
-  if PermanentRecord.db.profile.debug then
+function PermanentRecord:PR_LOG_WARN(...)
+  if self.db.profile.debug then
     print("[PermanentRecord]", ...)
   end
 end
 
-function PR_LOG_ERROR(...)
-  if PermanentRecord.db.profile.debug then
-    print("[PermanentRecord]", ...)
-  end
+function PermanentRecord:PR_LOG_ERROR(...)
+  print("[PermanentRecord]", ...)
 end
 
 function PermanentRecord:OnInitialize()
-  self.db = LibStub("AceDB-3.0"):New(addonName.."DB", defaults, true)
-  self.core = PermanentRecord.Core:New(self.db)
-  PermanentRecord:RegisterChatCommand("pr", "HandleSlashCmd")
+  self.db = LibStub("AceDB-3.0"):New(addonName .. "DB", defaults, true)
+end
+
+function PermanentRecord:OnEnable()
+  self:RegisterChatCommand("pr", "HandleSlashCmd")
+  self:PR_LOG_TRACE("PermanentRecord enable. Loaded", #self.db.profile.players, "player records.")
 end
 
 function PermanentRecord:HandleGroupEvent(event, ...)
-  PR_LOG_INFO(event)
+  self:PR_LOG_TRACE(event)
   C_Timer.After(2, function()
+    if PermanentRecord.Core == nil then
+      self:PR_LOG_ERROR("Core is not initialized, cannot handle group event:", event)
+      return
+    end
     if event == "GROUP_ROSTER_UPDATE" or event == "PLAYER_ENTERING_WORLD" then
-      self.core:ProcessGroupRoster()
+      self:ProcessGroupRoster()
     elseif event == "GROUP_JOINED" then
-      PR_LOG_INFO("Group joined")
+      self:PR_LOG_TRACE("Group joined")
     elseif event == "GROUP_LEFT" then
-      PR_LOG_INFO("Group left")
+      self:PR_LOG_TRACE("Group left")
     end
   end)
 end
@@ -70,108 +81,190 @@ PermanentRecord:RegisterEvent("GROUP_LEFT", "HandleGroupEvent")
 PermanentRecord:RegisterEvent("PLAYER_ENTERING_WORLD", "HandleGroupEvent")
 
 function PermanentRecord:HandleSlashCmd(input)
-  local parts = strsplittable(' ', input)
-  local command = parts[1] or ""
-  print("Input", input)
-  print("Command '".. command .."'")
-  print("parts2 '".. parts[2] .."'")
+  local args = strsplittable(' ', input)
+  local command = args[1] or ""
 
-  if command == "get" then
-    self:SlashGet(parts[2] or "")
-  elseif command == "add" then
-    self:SlashAdd(parts[2] or "")
+  if #args == 3 then
+    if command == "get" then
+      self:SlashGet(args)
+      return
+    elseif command == "add" then
+      self:SlashAdd(args)
+      return
+    elseif command == "remove" then
+      self:SlashRemove(args)
+      return
+    end
   elseif command == "debug" then
     self.db.profile.debug = not self.db.profile.debug
-  elseif command == "help" or command == "" then
-    print("Available commands:")
-    print("  pr get <player> - Get the record for a player")
-    print("  pr add <player> [flag] - Add a player with an optional flag (default is yellow)")
-    print("  pr help - Show this help message")
-  else
-    print("Unknown command. Type 'pr help' for available commands.")
-  end
-end
-
-function PermanentRecord:SlashGet(value)
-  local record = self.core:GetRecord(value)
-  if record then
-    print("Record for", value, "exists with flag:", record.flag)
-  else
-    print("No record found for", value)
-  end
-end
-
-function PermanentRecord:SlashAdd(player, flag)
-  flag = flag or PermanentRecord.Core.FLAG.Yellow
-  if player == "" then
-    print("Please provide a player name.")
+    print("Debug mode is now", self.db.profile.debug and "enabled" or "disabled")
+    return
+  elseif command == "st" or command == "status" then
+    print("PermanentRecord status:")
+    print("  Profile:", self.db.keys.profile)
+    print("  Debug mode:", self.db.profile.debug and "enabled" or "disabled")
+    print("  Loaded players:", #self.db.profile.players)
+    print("  Loaded guilds:", #self.db.profile.guilds)
     return
   end
-  self.core:AddRecord(player, flag)
+
+  print("Available commands:")
+  print("  pr get <player> - Get the record for a player")
+  print("  pr add <player> [flag] - Add a player with an optional flag (default is yellow)")
+  print("  pr help - Show this help message")
 end
 
-function PermanentRecord:SlashRemove(player)
-  local result = self.core:RemoveRecord(player)
-  if result then
-    print("Removed record for", player)
-  else
-    print("No record found for", player)
+function PermanentRecord:SlashGet(args)
+  local type = args[2] and args[2]:lower() or ""
+  local value = args[3]
+
+  if not type or type == "" then
+    self:PR_LOG_ERROR("Please specify 'p' for player or 'g' for guild.")
+    return
+  end
+  if not value or value == "" then
+    self:PR_LOG_ERROR("Please provide a name.")
+    return
+  end
+
+  if type == "p" or type == "player" then
+    self:GetPlayer(value)
+  elseif type == "g" or type == "guild" then
+    self:GetGuild(value)
   end
 end
 
+function PermanentRecord:SlashAdd(args, flag)
+  local type = args[2] and args[2]:lower() or ""
+  local value = args[3]
+  flag = flag or PermanentRecord.Core.FLAG.Yellow
 
+  if not type or type == "" then
+    self:PR_LOG_ERROR("Please specify 'p' for player or 'g' for guild.")
+    return
+  end
+  if not value or value == "" then
+    self:PR_LOG_ERROR("Please provide a name.")
+    return
+  end
 
--- function PR_IterateRoster(maxGroup,index)
--- 	index = (index or 0) + 1
--- 	maxGroup = maxGroup or 8
+  if type == "p" or type == "player" then
+    PermanentRecord:AddPlayer(value, flag)
+  elseif type == "g" or type == "guild" then
+    PermanentRecord:AddGuild(value, flag)
+  end
+end
 
--- 	if IsInRaid() then
--- 		if index > GetNumGroupMembers() then
--- 			return
--- 		end
--- 		local name, rank, subgroup, level, class, fileName, zone, online, isDead, role, isML, combatRole = GetRaidRosterInfo(index)
--- 		if subgroup > maxGroup then
--- 			return ExRT.F.IterateRoster(maxGroup,index)
--- 		end
--- 		local guid = UnitGUID(name or "raid"..index)
--- 		name = name or ""
--- 		return index, name, subgroup, fileName, guid, rank, level, online, isDead, combatRole
--- 	else
--- 		local name, rank, subgroup, level, class, fileName, online, isDead, combatRole, _
+function PermanentRecord:SlashRemove(args)
+  local type = args[2] and args[2]:lower() or ""
+  local value = args[3]
 
--- 		local unit = index == 1 and "player" or "party"..(index-1)
+  if not type or type == "" then
+    self:PR_LOG_ERROR("Please specify 'p' for player or 'g' for guild.")
+    return
+  end
 
--- 		local guid = UnitGUID(unit)
--- 		if not guid then
--- 			return
--- 		end
+  if type == "p" or type == "player" then
+    PermanentRecord:RemovePlayer(value)
+  elseif type == "g" or type == "guild" then
+    PermanentRecord:RemoveGuild(value)
+  end
+end
 
--- 		subgroup = 1
--- 		name, _ = UnitName(unit)
--- 		name = name or ""
--- 		if _ then
--- 			name = name .. "-" .. _
--- 		end
--- 		class, fileName = UnitClass(unit)
+---@enum Flag
+FLAG = {
+	Red = 0,
+	Yellow = 2,
+	Green = 4,
+}
 
--- 		if UnitIsGroupLeader(unit) then
--- 			rank = 2
--- 		else
--- 			rank = 1
--- 		end
+---@param name string Player name, with or without realm.
+---@return string|nil name Player name with realm if not provided, nil if name is empty
+local function GetNormalisedNameAndRealm(name)
+  if not name or name == "" then
+    return nil
+  end
+  if not name:find("-") then
+    local _, playerRealm = UnitFullName("player")
+    if playerRealm then
+      name = name .. "-" .. playerRealm
+    end
+  end
+  return string.lower(name)
+end
 
--- 		level = UnitLevel(unit)
+--- Processes group roster change events to check if any players have records.
+function PermanentRecord:ProcessGroupRoster()
+  if not IsInGroup() then
+    self:PR_LOG_TRACE("Not in a group, skipping roster processing.")
+    return
+  end
 
--- 		if UnitIsConnected(unit) then
--- 			online = true
--- 		end
+  self:PR_LOG_TRACE("Processing group roster update")
 
--- 		if UnitIsDeadOrGhost(unit) then
--- 			isDead = true
--- 		end
+  -- todo: add some locking mechanism to protect against race conditions
 
--- 		combatRole = UnitGroupRolesAssigned(unit)
+  local prefix  = IsInRaid() and "raid" or "party"
 
--- 		return index, name, subgroup, fileName, guid, rank, level, online, isDead, combatRole
--- 	end
--- end
+  local selfNameRealm = GetNormalisedNameAndRealm(GetUnitName("player", true))
+  for i = 1, GetNumGroupMembers() do
+    local playerNameRealm = GetUnitName(prefix..i, true)
+    if self:GetPlayer(playerNameRealm) then
+      self:PR_LOG_TRACE("I have seen this player before:", playerNameRealm)
+    elseif playerNameRealm ~= selfNameRealm then
+      self:AddPlayer(playerNameRealm, FLAG.Yellow)
+      self:PR_LOG_TRACE("Adding new record for player:", playerNameRealm)
+    end
+  end
+end
+
+---@param name string Player name, assumed to be the player's realm if not provided.
+---@param flag Flag Flag color.
+function PermanentRecord:AddPlayer(name, flag)
+  -- TODO: name doesn't include the home realm so the addon won't work across realms
+  if not name or name == "" then
+    self:PR_LOG_ERROR("Player name is required to add a record")
+    return
+  end
+  name = GetNormalisedNameAndRealm(name)
+  if self.db.profile.players[name] then
+    self:PR_LOG_ERROR("Error: Player ID already exists in records")
+    return
+  end
+  local record = {
+    playerId = name,
+    flag = flag,
+    history = {}
+  }
+  self.db.profile.players[name] = record
+end
+
+---@param name string Player name, assumed to be the player's realm if not provided.
+---@return table|nil record Record for the player, or nil if not found
+function PermanentRecord:GetPlayer(name)
+  if not name or name == "" then
+    self:PR_LOG_ERROR("Player name is required to get a record")
+    return nil
+  end
+  name = GetNormalisedNameAndRealm(name)
+  self:PR_LOG_TRACE("Getting record for player:", name)
+  return self.db.profile.players[name]
+end
+
+---@param name string Player name, assumed to be the player's realm if not provided.
+---@return boolean removed True if record was removed, false if no record found
+function PermanentRecord:RemovePlayer(name)
+  if not name or name == "" then
+    self:PR_LOG_ERROR("Player name is required to remove a record")
+    return false
+  end
+  name = GetNormalisedNameAndRealm(name)
+  if self.db.profile.players[name] then
+    self.db.profile.players[name] = nil
+    self:PR_LOG_TRACE("Removed record for player:", name)
+    return true
+  else
+    self:PR_LOG_ERROR("No record found for player:", name)
+    return false
+  end
+end
